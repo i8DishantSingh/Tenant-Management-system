f737ad34c82cdb51fe03fd6460a89f539783aaa6
i8DishantSingh <professional.id.dishant@gmail.com>
2026-06-18 23:20:49 +0530

    Structural changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,31 @@
 import express from "express";
-import propertyRoutes from "./routes/property.routes.js"; // Import your new routes
+import cors from "cors";
+import propertyRoutes from "./routes/property.routes.js";
+import tenantAppRoutes from "./routes/tenantApplication.routes.js";
+import invoiceRoutes from "./routes/invoice.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
+import { requireAuth } from "./middlewares/auth.middleware.js";
+import expenseRoutes from "./routes/expense.routes.js";
+
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
-app.use("/api/properties", propertyRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/properties", requireAuth, propertyRoutes);
+app.use("/api/applications", tenantAppRoutes);
+app.use("/api/invoices", invoiceRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/expenses", expenseRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "healthy", timestamp: new Date() });
